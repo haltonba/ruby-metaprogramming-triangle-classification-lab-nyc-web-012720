9d587324f1af6d5d57599669581facc816775c48
haltonba <haltonba94@gmail.com>
2020-02-08 18:31:43 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :side1, :side2, :side3
+
+  def initialize (side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def is_triangle?
+    if side1 == 0 || side2 == 0 || side3 == 0
+      return false
+    end
+    sorted = [side1, side2, side3].sort
+    longest_side = sorted.pop
+    longest_side < sorted.sum
+  end
+
+  def kind
+    if !is_triangle?
+      raise TriangleError
+    elsif [side1, side2, side3].uniq.length == 1
+      :equilateral
+    elsif [side1, side2, side3].uniq.length == 3
+      :scalene
+    else [side1, side2, side3].uniq.length == 2
+      :isosceles
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
